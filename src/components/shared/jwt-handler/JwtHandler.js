8e851d35d571ea490653f23e2ddf48cfa2c5bb58
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-10-19 17:33:53 -0300

    feat(jwtHandler): create JWT validation

---
+++ src/components/shared/jwt-handler/JwtHandler.js
@@ -0,0 +1,27 @@
+export const JwtHandler = {
+    JWT_KEY: "JWT",
+
+    onChangeEvent: new CustomEvent("onChangeJwt"),
+
+    onChange: () => {
+        window.dispatchEvent(JwtHandler.onChangeEvent);
+    },
+
+    setJwt: value => {
+        localStorage.setItem(JwtHandler.JWT_KEY, value);
+
+        JwtHandler.onChange();
+    },
+
+    getJwt: () => {
+        return localStorage.getItem(JwtHandler.JWT_KEY)
+    },
+
+    clearJwt: () => {
+        localStorage.removeItem(JwtHandler.JWT_KEY);
+
+        JwtHandler.onChange();
+    },
+
+    isJwtValid: () => Boolean(JwtHandler.getJwt()),
+}
