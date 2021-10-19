ddb629926688095e6fd8de449d4510f155b14dea
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-10-19 17:34:22 -0300

    feat(app): initial process

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Switch } from 'react-router';
 import './App.css';
+import GuardedRoute from './components/shared/GuardedRoute/GuardedRoute';
+import Header from './components/shared/Header/Header';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Logout from './pages/Logout/Logout';
+import "./styles/form.css"
 
 function App() {
   return (
     <div className="App">
-      APP
+      <Header/>
+      <div className="content">
+        <Switch>
+            <Route path="/" exact={true} component={Home}/>
+            <Route path="/login" component={Login}/>
+            <GuardedRoute path="/logout" component={Logout}/>
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ProfileList } from '../../components/structure/Profile/ProfileList/ProfileList'
+
+const Home = () => {
+    return (
+        <div>
+            <ProfileList></ProfileList>
+        </div>
+    )
+}
+
+export default Home
