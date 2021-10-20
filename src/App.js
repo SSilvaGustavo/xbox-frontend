53e6a2a52effd099c6b3075e827987928b9f5c8e
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-10-20 02:34:58 -0300

    feat(app): add new routes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 import GuardedRoute from './components/shared/GuardedRoute/GuardedRoute';
 import Header from './components/shared/Header/Header';
+import ViewGame from './pages/Games/ViewGames/ViewGames';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Logout from './pages/Logout/Logout';
+import ViewProfile from './pages/Profile/ViewProfile/ViewProfile';
+import CreateUser from './pages/User/CreateUser/CreateUser';
 import "./styles/card.css"
 
 
@@ -16,7 +19,11 @@ function App() {
         <Switch>
             <Route path="/" exact={true} component={Home}/>
             <Route path="/login" component={Login}/>
+            <GuardedRoute path="/guarded" component={Home} />
             <GuardedRoute path="/logout" component={Logout}/>
+            <GuardedRoute path="/profile/view/:id" component={ViewProfile}/>
+            <GuardedRoute path="/game/view/:id" component={ViewGame}/>
+            <Route path="/user/create" component={CreateUser}/>
         </Switch>
       </div>
     </div>
