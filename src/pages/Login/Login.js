f9d444370b09afd4d647745e69c47f96c4a1a632
SSilvaGustavo <gustavo.s.silva651@gmail.com>
2021-10-19 22:22:53 -0300

    feat(logib): Create and style login endpoint

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,48 +1,71 @@
+import { Link } from "react-router-dom";
 import { Api } from "../../api/Api";
 import { JwtHandler } from "../../components/shared/jwt-handler/JwtHandler";
-
+import "./Login.css"
 
 export default function Login(props) {
-    const handleSubmit = async event => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        const email = event.target.email.value
-        const password = event.target.password.value
-        
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
-        const payload = {
-            email,
-            password
-        };
-
-        const response = await Api.buildApiPostRequest(Api.loginUrl(), payload);
-
-        const result = await response.json();
-
-        if (response.status === 201){
-            const accessToken  = result.accessToken;
-
-            JwtHandler.setJwt({ accessToken });
-
-            props.history.push('/')
-        }else{
-
-        }
+    const payload = {
+      email,
+      password,
     };
 
+    const response = await Api.buildApiPostRequest(Api.loginUrl(), payload);
 
-    return(
-        <div>
-            <form className ="form" onSubmit={handleSubmit}>
-                    <div><label class="form__label" htmlFor="email">Email</label></div>
-                    <div><input className="form__input" type="text" id="email" name="email"/></div>
+    const result = await response.json();
 
-                    <div><label class="form__label" htmlFor="password">Password</label></div>
-                    <div><input className="form__input" type="text" id="password" name="password"/></div>
+    if (response.status === 201) {
+      const accessToken = result.accessToken;
 
-                    <div><input className="form__submit button--primary" type="submit" value="Login"/></div>
-            </form>
-        </div>
-    )
+      JwtHandler.setJwt({ accessToken });
 
+      props.history.push("/");
+    } else {
+    } 
+  };
+
+  return (
+    <div className="container">
+      <div className="form__card">
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="input__group">
+            <input
+              className="form__input"
+              type="text"
+              id="email"
+              name="email"
+              placeholder="E-mail"
+            />
+          </div>
+          <div className="input__group">
+            <input
+              className="form__input"
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+
+          <div className="input__group">
+            <input
+              className="form__submit"
+              type="submit"
+              value="Login"
+            />
+          </div>
+          <div  className="form__group"> 
+          <button  className="form__button"> 
+          <Link to="/">Create New Account</Link>
+          </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
